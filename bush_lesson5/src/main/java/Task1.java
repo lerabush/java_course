22fd48c0fdb0a012554c80d7b1061ee1c514b9c2
Valeria Bushueva <valeryabushuyeva@gmail.com>
2021-11-23 14:07:06 +0300

    added task 2

---
+++ bush_lesson5/src/main/java/Task1.java
@@ -0,0 +1,87 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        birthdays();
+        departureAndArrival();
+        schoolTime();
+        Boolean isTuesday = LocalDate.now().getDayOfWeek().equals(DayOfWeek.TUESDAY);
+        LocalTime timeMeeting = LocalTime.of(13, 30);
+        System.out.println(timeMeeting);
+        if (isTuesday && LocalTime.now().isBefore(timeMeeting))
+            System.out.println("Meeting is today!It's left: " + Duration.between(LocalTime.now(), timeMeeting).toHours());
+        else if (!isTuesday) System.out.println("Not today!");
+        else System.out.println("Meeting has already ended");
+
+    }
+
+    public static void birthdays() {
+        // Abe Lincoln's Birthday: February 12, 1809, died April 15, 1855
+        //   How old when he died?
+        //   How many days did he live?
+        System.out.println("Number 1");
+        LocalDate bDay = LocalDate.of(1809, 2, 12);
+        LocalDate dDay = LocalDate.of(1855, 4, 15);
+        System.out.println(Period.between(bDay, dDay).getYears());
+        System.out.println(DAYS.between(bDay, dDay));
+        // Bennedict Cumberbatch, July 19, 1976
+        //   Born in a leap year?
+        //   How many days in the year he was born?
+        //   How many decades old is he?
+        //   What was the day of the week on his 21st birthday?
+        System.out.println("Number 2");
+        bDay = LocalDate.of(1976, 7, 19);
+        System.out.println("is leap year? " + bDay.isLeapYear());
+        System.out.println(bDay.isLeapYear() ? 366 : 365 + " days");
+        System.out.println(Period.between(bDay, LocalDate.now()).getYears() / 10);
+        LocalDate bDay21 = LocalDate.of(1997, 7, 19);
+        System.out.println(bDay21.getDayOfWeek());
+    }
+
+    public static void schoolTime() {
+        // School semester starts the second Tuesday of September of this year.
+        //   Hint: Look at the TemporalAdjusters class
+        //   What is the date?
+        //   School summer vacation starts June 25th
+        //   Assuming:
+        //     *  Two weeks off in December
+        //     *  Two other vacation weeks
+        //     *  School is taught Monday - Friday
+        //   How many days of school are there?
+        //   Hint: keep track of the short weeks also
+        System.out.println("Number 5");
+        LocalDateTime schoolStarts = LocalDate.of(LocalDate.now().getYear(), Month.SEPTEMBER, 1).atStartOfDay()
+                .with(TemporalAdjusters.firstInMonth(DayOfWeek.TUESDAY));
+
+        System.out.println(schoolStarts.plusDays(7));
+        LocalDateTime finishSemester = LocalDate.of(LocalDate.now().getYear() + 1, Month.JUNE, 25).atStartOfDay();
+        long daysInSchool = Duration.between(schoolStarts.atOffset(ZoneOffset.UTC), finishSemester.atOffset(ZoneOffset.UTC)).toDays();
+        System.out.println((daysInSchool / 7 - 4) * 5);
+    }
+
+    public static void departureAndArrival() {
+        // Train departs Boston at 1:45PM and arrives New York 7:25PM
+        //   How many minutes long is the train ride?
+        //   If the train was delayed 1 hour 19 minutes, what is the actual arrival time?
+        System.out.println("Number 3");
+        LocalTime timeDepart = LocalTime.of(13, 45);
+        LocalTime timeArrival = LocalTime.of(19, 45);
+        System.out.println(Duration.between(timeDepart, timeArrival).toMinutes());
+        System.out.println(timeArrival.plusHours(1).plusMinutes(19));
+        // Flight: Boston to Miami, leaves March 24th 9:15PM. Flight time is 4 hours 15 minutes
+        //   When does it arrive in Miami?
+        //   When does it arrive if the flight is delays 4 hours 27 minutes?
+        System.out.println("Number 4");
+        LocalDateTime timeDate = LocalDate.of(LocalDate.now().getYear(), 3, 24).atTime(21, 15);
+        LocalDateTime arrivalDate = timeDate.plusHours(4).plusMinutes(15);
+        System.out.println(arrivalDate);
+        System.out.println(arrivalDate.plusHours(4).plusMinutes(27));
+    }
+}
---
+++ bush_lesson5/src/main/java/Task2.java
@@ -0,0 +1,70 @@
+import java.time.*;
+import java.time.temporal.TemporalAdjusters;
+
+public class Task2 {
+    public static void main(String[] args) {
+        timeZoneExamples();
+    }
+
+    public static void timeZoneExamples() {
+        ZoneId BLR = ZoneId.of("Asia/Calcutta");
+        ZoneId SFO = ZoneId.of("America/New_York");
+        ZoneId BOS = ZoneId.of("America/Los_Angeles");
+
+        //Flight 123, San Francisco to  Boston, leaves SFO at 10:30 PM June 13, 2014
+        // The flight is 5 hours 30 minutes
+        //   What is the local time in Boston when the flight takes off?
+        //   What is the local time at Boston Logan airport when the flight arrives?
+        //   What is the local time in San Francisco when the flight arrives?
+        System.out.println("Number 1");
+        LocalDateTime startFromSanFr = LocalDate.of(2014, Month.JUNE, 13).atTime(22, 30);
+        ZonedDateTime startFromSanFrZone = ZonedDateTime.of(startFromSanFr, SFO);
+        LocalTime flightTime = LocalTime.of(5, 30);
+
+        System.out.println("What is the local time in Boston when the flight takes off?  " + startFromSanFrZone.toOffsetDateTime()
+                .atZoneSameInstant(BOS).toLocalTime());
+        System.out.println("What is the local time at Boston Logan airport when the flight arrives? " + (startFromSanFrZone.plusHours(flightTime.getHour())
+                .plusMinutes(flightTime.getMinute())).toOffsetDateTime()
+                .atZoneSameInstant(BOS).toLocalTime());
+        System.out.println("What is the local time in San Francisco when the flight arrives? " + startFromSanFrZone.
+                plusHours(flightTime.getHour()).plusMinutes(flightTime.getMinute()).toLocalTime());
+        // Flight 456, San Francisco to Bangalore, India, leaves SFO at Saturday, 10:30 PM June 28, 2014
+        // The flight time is 22 hours
+        //   Will the traveler make a meeting in Bangalore Monday at 9 AM local time?
+        //   Can the traveler call her husband at a reasonable time when she arrives?
+        System.out.println("Number 2");
+        startFromSanFr = LocalDate.of(2014, Month.JUNE, 28).atTime(22, 30);
+        startFromSanFrZone = ZonedDateTime.of(startFromSanFr, SFO);
+        flightTime = LocalTime.of(22, 0);
+        LocalDateTime meeting = LocalDate.of(startFromSanFr.getYear(), startFromSanFr.getMonth(),
+                startFromSanFr.with(TemporalAdjusters.
+                        next(DayOfWeek.MONDAY)).getDayOfMonth()).atTime(9, 0);
+        ZonedDateTime zonedMeetingBLR = ZonedDateTime.of(meeting, BLR);
+        System.out.println("Will the traveler make a meeting in Bangalore Monday at 9 AM local time? " + startFromSanFrZone.plusHours(flightTime.getHour())
+                .toOffsetDateTime().atZoneSameInstant(BLR).isBefore(zonedMeetingBLR));
+        System.out.println("Can the traveler call her husband at a reasonable time when she arrives? ");
+        LocalTime travelerTime = startFromSanFrZone.plusHours(flightTime.getHour())
+                .toOffsetDateTime().atZoneSameInstant(BLR).toLocalTime();
+        LocalTime husbandTime = startFromSanFrZone.plusHours(flightTime.getHour())
+                .toOffsetDateTime().toLocalTime();
+        System.out.println("Traveler's local time:" + travelerTime);
+        System.out.println("Husband's local time: " + husbandTime);
+        if (travelerTime.getHour() > 8 && travelerTime.getHour() < 23
+                && husbandTime.getHour() > 8 && husbandTime.getHour() < 23)
+            System.out.println("Yes");
+        else System.out.println("No:(");
+        // Flight 123, San Francisco to Boston, leaves SFO at 10:30 PM Saturday, November 1st, 2014
+        // Flight time is 5 hours 30 minutes.
+        //   What day and time does the flight arrive in Boston?
+        //   What happened?
+        System.out.println("Number 3");
+        startFromSanFr = LocalDate.of(2014, Month.NOVEMBER,1).atTime(22,30);
+        flightTime = LocalTime.of(5,30);
+        startFromSanFrZone = ZonedDateTime.of(startFromSanFr,SFO);
+        System.out.println("What day and time does the flight arrive in Boston? "+startFromSanFrZone.plusHours(flightTime.getHour())
+                .plusMinutes(flightTime.getMinute()).toOffsetDateTime().atZoneSameInstant(BOS).toLocalDateTime());
+
+
+    }
+
+}
