0225368659aa2594f6830af63dfc0a1562fdf21a
Valeria Bushueva <valeryabushuyeva@gmail.com>
2021-12-07 00:23:04 +0300

    added DirFind

--- bush_lesson6/src/main/java/homework/task2/DirFind.java
+++ bush_lesson6/src/main/java/homework/task2/DirFind.java
@@ -1,18 +1,24 @@
 package homework.task2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class DirFind {
     
     public static void main(String[] args) {
 
-/* Remove comments
-        try{ // Add try with resources
+
+        try(Stream<Path> files = Files.find(Paths.get(args[0]),5,((path, basicFileAttributes) -> String.valueOf(path).endsWith(".txt")))){ // Add try with resources
             
             System.out.println("\n=== Find all dirs ===");
-            // Print out directory list here
+            files.forEach(System.out::println);
                 
-        }catch (IOException e){
-            System.out.println("Error: " + e.getMessage());
+        }catch ( IOException ex){
+            System.out.println("Error: " + ex.getMessage());
         }
-*/
+
     } 
 }
